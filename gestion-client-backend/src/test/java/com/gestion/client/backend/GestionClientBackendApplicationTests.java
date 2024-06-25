e4525c32043b1fe919082db7c0aa64d4611d8643
Emmanuel Martillo <fernonmartillo@gmail.com>
2024-06-25 00:50:36 -0500

    [NUEVO][PROYECTO] Login Crud con Spring Boot y React .JS

---
+++ gestion-client-backend/src/test/java/com/gestion/client/backend/GestionClientBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.client.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClientBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
